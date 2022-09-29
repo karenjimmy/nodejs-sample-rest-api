995635c95e34cd9da85c61cebe2b43e8e2cc5f9d
unknown <karenjimmy2@gmail.com>
2022-09-29 13:34:08 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+
+app.get("/", (req, res) => {
+  res.sendFile("/views/index.html", { root: __dirname });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
